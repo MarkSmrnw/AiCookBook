7c06b8d6b040f79a668e333e30e786d827ac1962
Mark <107583221+MarkSmrnw@users.noreply.github.com>
2025-06-30 22:24:54 +0200

    Projekterstellung.

---
+++ INTERFACE/source/JS/reciperotation.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function showDishes() {
+        const food = [
+            "Spaghetti", 
+            "Pizza", 
+            "Sushi", 
+            "Tacos", 
+            "Burgers", 
+            "Salad", 
+            "Curry", 
+            "Pasta", 
+            "Steak", 
+            "Soup", 
+            "Ramen"
+        ]
+        const images = [
+            "source/media/images/spaghetti.webp",
+            "source/media/images/pizza.webp",
+            "source/media/images/sushi.webp",
+            "source/media/images/tacos.webp",
+            "source/media/images/burger.webp",
+            "source/media/images/salad.webp",
+            "source/media/images/curry.webp",
+            "source/media/images/steak.webp",
+            "source/media/images/ramen.webp"
+        ]
+        const descriptions = [
+            "A classic Italian pasta dish made with durum wheat semolina.",
+            "A popular dish consisting of a flat round base topped with various ingredients.",
+            "A Japanese dish of vinegared rice accompanied by various ingredients, such as seafood and vegetables.",
+            "A traditional Mexican dish consisting of a folded or rolled tortilla filled with various ingredients.",
+            "A sandwich consisting of a cooked patty of ground meat, usually beef, placed inside a sliced bun.",
+            "A dish consisting of mixed raw or cooked vegetables, often served with a dressing.",
+            "A South Asian dish made with meat or vegetables cooked in a spiced sauce, typically served with rice.",
+            "A cut of meat that is grilled or fried, often served with sides like potatoes or vegetables.",
+            "A Japanese noodle soup consisting of Chinese-style wheat noodles served in a meat- or fish-based broth."
+        ]
+
+        // TO DO: [MARK] 
+        //
+    }
+});
---
+++ INTERFACE/source/media/images/_info.md
@@ -0,0 +1,5 @@
+# INFO TO THE USED PICTURED
+
+This product is not ment to procude revenue.
+
+All pictures are free of copyright and have been downloaded from pixabay
